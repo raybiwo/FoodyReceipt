70b03a6644c6319a89d6e1afdc0d02b84e75e340
raybiwo <ahmad@firstpayment.co.id>
2020-12-31 02:54:44 +0700

    inject remote data source

---
+++ app/src/main/java/com/biwo/foodyreceipt/RemoteDataSource.kt
@@ -0,0 +1,14 @@
+package com.biwo.foodyreceipt
+
+import com.biwo.foodyreceipt.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
+}
