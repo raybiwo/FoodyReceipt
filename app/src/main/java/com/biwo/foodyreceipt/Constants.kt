333ae1453c471d38f96769598176a7038ac821fb
raybiwo <ahmad@firstpayment.co.id>
2020-12-30 19:13:34 +0700

    create constant and adding package

---
+++ app/src/main/java/com/biwo/foodyreceipt/Constants.kt
@@ -0,0 +1,9 @@
+package com.biwo.foodyreceipt
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "31ba5b9387d54bd9907668fa60955558"
+    }
+}
--- app/src/main/java/com/biwo/foodyreceipt/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/biwo/foodyreceipt/ui/fragments/recipes/RecipesFragment.kt
@@ -1,17 +1,24 @@
-package com.biwo.foodyreceipt
+package com.biwo.foodyreceipt.ui.fragments.recipes
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.biwo.foodyreceipt.R
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
-class FavoriteRecipesFragment : Fragment() {
+class RecipesFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        val view =  inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        view.recyclerview.showShimmer();
+        return view;
     }
+
 }
